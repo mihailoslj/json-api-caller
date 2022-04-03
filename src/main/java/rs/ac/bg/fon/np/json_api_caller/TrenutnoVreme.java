249dc6acd2b52335fdb965f57809e73836fa5794
mihailoslj <samozagit@gmail.com>
2022-04-03 19:54:45 +0200

    Kreirana domenska klasa TrenutnoVreme, get/set metode i toString za sve njene atribute

---
+++ src/main/java/rs/ac/bg/fon/np/json_api_caller/TrenutnoVreme.java
@@ -0,0 +1,104 @@
+package rs.ac.bg.fon.np.json_api_caller;
+
+import java.lang.invoke.StringConcatFactory;
+import java.sql.Date;
+import com.google.gson.annotations.SerializedName;
+
+public class TrenutnoVreme {
+	@SerializedName("Grad")
+	private String name;
+	
+	@SerializedName("Drzava")
+	private String country;
+	
+	@SerializedName("Temperatura")
+	private int temperature;
+	
+	@SerializedName("Geo. sirina")
+	private double lat;
+	
+	@SerializedName("Geo. duzina")
+	private double lon;
+	
+	@SerializedName("Trenutno vreme")
+	private Date localtime;
+	
+	@SerializedName("Vlaznost")
+	private int humidity;
+	
+	@SerializedName("Vaz. pritisak")
+	private int pressure;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public int getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(int temperature) {
+		this.temperature = temperature;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLon() {
+		return lon;
+	}
+
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+
+	public Date getLocaltime() {
+		return localtime;
+	}
+
+	public void setLocaltime(Date localtime) {
+		this.localtime = localtime;
+	}
+
+	public int getHumidity() {
+		return humidity;
+	}
+
+	public void setHumidity(int humidity) {
+		this.humidity = humidity;
+	}
+
+	public int getPressure() {
+		return pressure;
+	}
+
+	public void setPressure(int pressure) {
+		this.pressure = pressure;
+	}
+
+	@Override
+	public String toString() {
+		return "TrenutnoVreme [name=" + name + ", country=" + country + ", temperature=" + temperature + ", lat=" + lat
+				+ ", lon=" + lon + ", localtime=" + localtime + ", humidity=" + humidity + ", pressure=" + pressure
+				+ "]";
+	}
+	
+	
+}
